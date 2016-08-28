81b8f1fb0cc8b8dc2e939c279c0912c6302af571
Vakula7 <vakulasona96@gmail.com>
2016-08-28 08:10:20 +0530

    Create Sampl.java

---
+++ Sampl.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+import java.io.*;
+public class Sampl
+{
+public static void main(String args[])
+{
+String s1="bfdf";
+Scanner s=new Scanner(System.in);
+s1=s.nextLine();
+StringBuffer buf=new StringBuffer(s1);
+System.out.println(buf.reverse());
+}
+}
